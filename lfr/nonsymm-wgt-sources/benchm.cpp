539f1dda6c155d0ec227d8da645908c71fd3dbb6
Dimitris Floros <fcdimitr@gmail.com>
2024-02-24 22:52:01 +0200

    fix benchmark

--- lfr/nonsymm-wgt-sources/benchm.cpp
+++ lfr/nonsymm-wgt-sources/benchm.cpp
@@ -835,12 +835,12 @@ double* benchmark(bool excess, bool defect, int num_nodes,
 
 		if (nmin>nmax) {
 			cerr<<"\n***********************\nERROR: INVERTED COMMUNITY SIZE BOUNDS"<<endl;
-			return false;
+			return NULL;
 		}
 
 		if(nmax>num_nodes) {
 			cerr<<"\n***********************\nERROR: maxc BIGGER THAN THE NUMBER OF NODES"<<endl;
-			return false;
+			return NULL;
 		}
 
 
